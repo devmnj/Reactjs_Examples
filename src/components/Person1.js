08211dd611d20cb5df775f82895cb9b725384fbb
Manoj AP <manojap.nair@gmail.com>
2020-11-21 18:51:05 +0530

    Projects added

--- src/components/Person1.js
+++ src/components/Person1.js
@@ -1,27 +1,39 @@
-
-import React,{Component} from 'react'
-import Person2 from './Person2'
+import React, { Component } from "react";
+import Person2 from "./Person2";
 class Person1 extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-             greetTxt:''
-        }
-this.greetPerson1=this.greetPerson1.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      greetTxt: "",
+    };
+  
+  }
 
-    greetPerson1(greet){
-        this.setState({greetTxt:greet})
-    }
-    render() {
-        return (
-            <div>
-                <h4 >{this.props.name}</h4>
-                  He/She told : {this.state.greetTxt}
-                <Person2 name='Adithi' rel='Father' greetHim={this.greetPerson1} />
-                <Person2 name='Sheena' rel='Brother' greetHim={this.greetPerson1} />
-            </div>
-        )
-    }
+ 
+//As a class property
+  greetPerson1=(greet)=> {
+    this.setState({ greetTxt: greet });
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>{this.props.name}</h4>
+        He/She told : {this.state.greetTxt}
+  
+<Person2
+          name="Adithi"
+          rel="Father"
+          greetHim={this.greetPerson1}
+        />
+        <Person2
+          name="Sheena"
+          rel="Brother"
+          greetHim={this.greetPerson1}
+        />
+
+      </div>
+    );
+  }
 }
-export default Person1
+export default Person1;
